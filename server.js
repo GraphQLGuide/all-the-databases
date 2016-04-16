a6aedd3bf5c1362f5c60eba5688916d882716926
Jonas Helfer <helfer@meteor.com>
2016-04-16 00:52:13 -0700

    move mocks to a separate file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { apolloServer } from 'graphql-tools';
 import Schema from './data/schema';
+import Mocks from './data/mocks';
 
 const GRAPHQL_PORT = 8080;
 
@@ -9,7 +10,7 @@ graphQLServer.use('/', apolloServer({
   graphiql: true,
   pretty: true,
   schema: Schema,
-  mocks: { String: () => 'It works!' },
+  mocks: Mocks,
 }));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
