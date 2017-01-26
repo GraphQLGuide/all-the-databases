6b2afdd80b0796e300d88d124399bf0a82fd6893
Loren Sands-Ramshaw <lorensr@gmail.com>
2017-01-25 19:02:42 -0500

    Empty the resolvers

--- data/resolvers.js
+++ data/resolvers.js
@@ -4,56 +4,26 @@ import { User, Tweet, Views, Elasticsearch, redis } from './connectors';
 const resolvers = {
   Query: {
     user(_, args) {
-      return User.find({
-        where: { id: args.id },
-      });
+      return null;
     },
     async publicFeed() {
-      const feed = await redis.lrangeAsync('public_feed', 0, -1);
-      return feed.map(JSON.parse);
+      return [];
     },
     async cityFeed(_, args, context) {
-      const response = await rp(`http://ipinfo.io/${context.ip}`);
-      const { city } = JSON.parse(response);
-      // {
-      //   "ip": "8.8.8.8",
-      //   "hostname": "google-public-dns-a.google.com",
-      //   "city": "Mountain View",
-      //   "region": "California",
-      //   "country": "US",
-      //   "loc": "37.3860,-122.0838",
-      //   "org": "AS15169 Google Inc.",
-      //   "postal": "94040"
-      // }
-
-      const cityTweets = await Tweet.findAll({
-        where: { city },
-        limit: 3,
-        order: [['created', 'DESC']],
-      });
-
-      return cityTweets;
+      return [];
     },
   },
   User: {
     async mentions(user) {
-      const results = await Elasticsearch.search({ q: `${user.firstName} ${user.lastName}` });
-      return results.hits.hits.map(hit => Tweet.build({ ...hit._source, id: hit._id }));
+      return [];
     },
   },
   Tweet: {
     author(tweet) {
-      // in Redis, author is a subdoc
-      if (tweet.user) {
-        return tweet.user;
-      }
-
-      return tweet.getUser();
+      return null;
     },
     views(tweet) {
-      return Views
-        .findOne({ tweetId: tweet.id })
-        .then(doc => doc.views);
+      return null;
     },
   },
 };
